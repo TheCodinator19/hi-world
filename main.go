ab3f60f103c507723e252faea41d1abca43cdce9
Sarah Conord <sarahzconord@gmail.com>
2020-04-20 15:33:35 -0400

    ssh key additions

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	cowsay "github.com/Code-Hex/Neo-cowsay"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/ping", pingHandler)
+
+	r.GET("/cow/:phrase", cowHandler)
+
+	r.Run(":8080")
+}
+
+func pingHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func cowHandler(c *gin.Context) {
+	phrase := c.Param("phrase")
+	say, _ := cowsay.Say(cowsay.Phrase(phrase), cowsay.Type("beavis.zen"))
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"title":  "Hi Tommy",
+		"phrase": say, //just updating for SSH
+	})
+}
